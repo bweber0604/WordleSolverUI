d7fac69812c161058dd19d3ccf1b5ea5847eb910
bweber0604 <70308026+bweber0604@users.noreply.github.com>
2022-04-11 23:12:54 -0400

    updated UI added further substitution
    added a system to only show the words that can show up in a wordle answer. while wordmaster uses a larger variety of words that don't appear in normal wordle. removed unused code.

--- src/wordleSolver/WordleMain.java
+++ src/wordleSolver/WordleMain.java
@@ -1,4 +1,6 @@
 package wordleSolver;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
@@ -29,64 +31,59 @@ public class WordleMain extends GBFrame {
 	JLabel title = addLabel("Wordle Solver: ", 1, 1, 1, 1);
 	JLabel prev = addLabel("Previous Guesses: ", 1, 2, 1, 1);
 	
-	JTextArea words = addTextArea("", 2, 1, 1, 3);
+	JComboBox type = addComboBox(2, 1, 1, 1);
 	
-	JLabel word = addLabel("Type Word Here: ", 2, 2, 1, 1);
-	JTextField userInput = addTextField("", 2, 3, 1, 1);
+	JTextArea words = addTextArea("", 3, 1, 1, 3);
 	
-	JLabel wordRes = addLabel("Type Word Results Here: ", 3, 2, 1, 1);
-	JTextField userInputResults = addTextField("", 3, 3, 1, 1);
+	JLabel word = addLabel("Type Word Here: ", 3, 2, 1, 1);
+	JTextField userInput = addTextField("", 3, 3, 1, 1);
 	
-	JButton search = addButton("Search", 4, 3, 1, 1);
+	JLabel wordRes = addLabel("Type Word Results Here: ", 4, 2, 1, 1);
+	JTextField userInputResults = addTextField("", 4, 3, 1, 1);
 	
-	JButton reset =addButton("Reset", 4, 4, 1, 1);
+	JButton search = addButton("Search", 5, 3, 1, 1);
+	
+	JButton reset =addButton("Reset", 5, 4, 1, 1);
 	
 	ArrayList<String> possibleWords = new ArrayList<String>();
-		
+	ArrayList<String> possibleAnswers = new ArrayList<String>();	
+	
 	String userWord = "";
 	String wordResults = "";
 	
-	//boolean running = true;
-	
-	
-	//breaks with double vowels
-	
+	boolean wordMaster = false;
+
+
 	public void buttonClicked(JButton btn) {
 		if(btn == search) {
 			userWord = userInput.getText().trim();
 			wordResults = userInputResults.getText().trim();
 			
-			prev.setText(prev.getText() + userWord);
+			prev.setText(prev.getText().concat(userWord + " "));
 			
 			String fullList = "";
 			
-			compareWords();
-			for(String s : possibleWords) {
-				fullList += s + "\n";
+			if(wordResults.equals("*****")) {
+				messageBox("Correct Word Guessed");
+				filterWords();
 			}
 			
+			compareWords();
+			for(int i = 0; i < possibleWords.size(); i++) {
+				fullList += possibleWords.get(i) + "\n";
+				
+			}
 			words.setText(fullList);
-			
+						
 			userInput.setText("");
 			userInputResults.setText("");
 		}else if(btn == reset) {
-			resetList();
+			prev.setText("");
+			filterWords();
 		}
 	}
 	
-	
-	public void getInputs() {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your word: ");
-		userWord = sc.nextLine();
-		System.out.println("Enter wordle results: ");
-		wordResults = sc.nextLine();
-	}
-	
 	public void compareWords() {
-		
-		
-		//trees /*/--
 		
 			int ind = wordResults.indexOf('*');
 
@@ -98,8 +95,9 @@ public class WordleMain extends GBFrame {
 						possibleWords.remove(s);
 						i--;//prevent array from skipping elements
 					}
+					
 				}
-
+				
 				ind = wordResults.indexOf('*', ind+1);
 			}
 			
@@ -115,19 +113,59 @@ public class WordleMain extends GBFrame {
 						possibleWords.remove(s);
 						i--;
 					}
+					
 				}
 				
 				ind = wordResults.indexOf('/', ind+1);
 			}
 			
+			
+			//System.out.println(nonMutable.toString());
+			//breaks with double vowels
+			//repeat letters break program
+			
+			//double letters count only as one instance of word
+			//words with one letter of double letters in word
+			//program removes all words that dont have the letter
+			//then after proceeding to "-" then it remove all the words with the second double letter making the possible word array empty
 			ind = wordResults.indexOf('-');
 			
 			while(ind != -1) {
 				for(int i = 0; i < possibleWords.size(); i++) {//check every word in array
 					String s = possibleWords.get(i);
-					if(s.indexOf(userWord.charAt(ind)) != -1) {//-1 means no letter present if it is not -1 means it is in word
-						possibleWords.remove(s);
-						i--;//stops array element skipping
+					char temp = userWord.charAt(ind);//stores the character that is being checked to be removed
+					if(s.indexOf(temp) != -1) {//-1 means no letter present if it is not -1 means it is in word
+						int cInd = wordResults.indexOf('/');
+						boolean doubleLetter = false;
+						
+						char l = userWord.charAt(cInd);
+						//need a check for double letters from previous sections above
+						
+						while(cInd != -1) {//loop until there are no more /
+							if(s.charAt(cInd) == temp) {// if index of / is the same character as the letter to be removed (presence of double letters) dont remove that letter
+								doubleLetter = true;
+								break;//double letter found. now dont remove word 
+							}
+							
+							cInd = wordResults.indexOf('/', cInd +1);
+						}
+						
+						
+						
+//						while(cInd != -1) {
+//							if(s.indexOf(userWord.charAt(cInd)) != -1 && userWord.charAt(cInd) == s.charAt(ind)) {// there is a presense of a /
+//								//System.out.println(userWord.charAt(cInd) + " " + s.charAt(ind));
+//								System.out.println(s);
+//								doubleLetter = true;
+//								break;
+//							}
+//							cInd = wordResults.indexOf('/', cInd+1);
+//						}
+						if(!doubleLetter) {
+							//System.out.println(s);
+							possibleWords.remove(s);
+							i--;//stops array element skipping	
+						}
 					}
 				}
 				
@@ -142,6 +180,7 @@ public class WordleMain extends GBFrame {
 		try {
 			File words = new File("words.txt");//init file model
 			Scanner reader = new Scanner(words);
+			possibleWords.clear();
 			
 			while(reader.hasNextLine()) {//read through every line of array
 				String data = reader.nextLine();
@@ -153,30 +192,59 @@ public class WordleMain extends GBFrame {
 		}
 	}
 	
-	public WordleMain() {
-		words.setEditable(false);
+	public void filterWords() {
+		resetList();
 		
-		try {
-			File words = new File("words.txt");//init file model
-			Scanner reader = new Scanner(words);
-			
-			while(reader.hasNextLine()) {//read through every line of array
-				String data = reader.nextLine();
-				possibleWords.add(data);//add to array
+		if(!wordMaster) {
+			try {
+				File words = new File("answers.txt");//init file model
+				Scanner reader = new Scanner(words);
+				possibleAnswers.clear();
+				
+				while(reader.hasNextLine()) {//read through every line of array
+					String data = reader.nextLine();
+					possibleAnswers.add(data.toLowerCase());//add to array
+				}
+				reader.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("Error");
 			}
-			reader.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("Error");
+			
+			for(int i = 0; i< possibleWords.size(); i++) {
+				if(!possibleAnswers.contains(possibleWords.get(i))) {//get only the words that can be answers
+					possibleWords.remove(possibleWords.get(i));
+					i--;
+				}
+			}
 		}
 		
 		
-//		while(running) {
-//			getInputs();
-//			compareWords();
-//			for(String s : possibleWords) {
-//				System.out.println(s);
-//			}
-//		}
+		
+	}
+	
+	
+	
+	public WordleMain() {
+		words.setEditable(false);
+		
+		type.addItem("Wordle");
+		type.addItem("Word Master");
+		
+		type.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(type.getSelectedItem().equals("Wordle")) {
+					wordMaster = false;
+					filterWords();
+				}else {
+					wordMaster = true;
+					filterWords();
+				}
+			}
+		});
+		
+		filterWords();
 		
 	}
 	
